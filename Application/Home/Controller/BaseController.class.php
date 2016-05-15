5c43eac0cdca7deaf50ae74bc0fa0753aec33570
wangaojie <15010153015@163.com>
2016-05-15 23:24:30 +0800

    qddd

---
+++ Application/Home/Controller/BaseController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class BaseController extends Controller {
+	public $page = 1;
+	public $limit = 10;
+    
+}
---
+++ Application/Home/Controller/NoticeController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NoticeController extends BaseController {
+	
+    public function index(){
+    	$page = $_REQUEST['page'] ? $_REQUEST['page'] : 1;
+    	$page = ($page - 1) * $this->limit;
+    	$list = M('notice')->limit($page,$this->limit)->field('notice_id,notice_title,user_id,notice_abstract,FROM_UNIXTIME(add_time,"%Y-%m-%d %H:%i:%s") add_time')->select();
+    	output_data($list);
+    }
+    
+}
